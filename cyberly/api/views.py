60a61dc084bf4ffd51ba6b8e5d93fb9bff07949f
Camilo Arenas <camilovamp@gmail.com>
2025-05-12 00:22:22 -0400

    readme, apis, postman collection

--- cyberly/api/views.py
+++ cyberly/api/views.py
@@ -1,10 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, DjangoFilterBackend
 from profiles.models import Profile, ProfileCategory, ProfileLanguage
 from .serializers import ProfileSerializer, ProfileCategorySerializer, ProfileLanguageSerializer
+from .filters import ProfileFilter
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProfileFilter
 
 class ProfileCategoryViewSet(viewsets.ModelViewSet):
     queryset = ProfileCategory.objects.all()
