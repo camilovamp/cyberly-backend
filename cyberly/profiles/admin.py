20fd27cb7e32dd5b7a12c6452ff4decf4a880a8a
Camilo Arenas <camilovamp@gmail.com>
2025-05-11 21:56:15 -0400

    add admin and load data for categories and languages

--- cyberly/profiles/admin.py
+++ cyberly/profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Language, Category
 
-# Register your models here.
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_featured']
+    search_fields = ['name']
+    list_filter = ['is_featured']
+    ordering = ['name']
